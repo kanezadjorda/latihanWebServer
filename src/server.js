5e7863efe55308c02c76cd7ec0683198c194fbf3
projectbersamabarokah <projectbersamabarokah@gmail.com>
2025-11-02 17:01:06 +0700

    Enable CORS for all routes

--- src/server.js
+++ src/server.js
@@ -2,20 +2,23 @@ const Hapi = require('@hapi/hapi');
 const routers = require('./routes');
 
 const init = async () => {
-	const server = Hapi.server({
-		port: 5000,
-		host: '0.0.0.0',
-		routes: {
-			cors: {
-				origin: ['*'],
-			},
-		},
-	});
+  const server = Hapi.server({
+    port: 5000,
+    host: '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+        headers: ['Accept', 'Authorization', 'Content-Type', 'If-None-Match'],
+        additionalHeaders: ['cache-control', 'x-requested-with'],
+        credentials: true,
+      },
+    },
+  });
 
-	server.route(routers);
+  server.route(routers);
 
-	await server.start();
-	console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
