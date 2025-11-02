6252dd39e649e1553feb77aa97c60049a8a130a4
projectbersamabarokah <projectbersamabarokah@gmail.com>
2025-11-02 19:37:57 +0700

    apa aja dah

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const routers = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
+    // eslint-disable-next-line no-undef
     port: process.env.PORT || 5000,
     host: '0.0.0.0',
     routes: {
@@ -21,8 +22,10 @@ const init = async () => {
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
+// eslint-disable-next-line no-undef
 process.on('unhandledRejection', (err) => {
   console.log(err);
+  // eslint-disable-next-line no-undef
   process.exit(1);
 });
 
